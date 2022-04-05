9f013d0146d3a0010c4d6aa8721f5e79d2abb6ae
nielsoffice <nieldigitalsolution@gmail.com>
2022-04-05 19:24:55 +0800

    wp action private

--- wp/qualitytropicalexports/src/includes/class-qualitytropicalexports.php
+++ wp/qualitytropicalexports/src/includes/class-qualitytropicalexports.php
@@ -153,22 +153,6 @@ class Qualitytropicalexports {
 	}
 
 	/**
-	 * Register all of the hooks related to the public-facing functionality
-	 * of the plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 */
-	private function define_public_hooks() {
-
-		$plugin_public = new Qualitytropicalexports_Public( $this->get_plugin_name(), $this->get_version() );
-
-		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
-		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
-
-	}
-
-	/**
 	 * Run the loader to execute all of the hooks with WordPress.
 	 *
 	 * @since    1.0.0
